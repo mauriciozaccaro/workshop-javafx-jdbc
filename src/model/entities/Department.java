cd7ea6a88876ccc64b0a357b6e0f42f512a07685
Mauricio PC Casa <mauriciozaccaro@gmail.com>
2021-07-08 21:59:23 -0400

    DepartmentList Controller

---
+++ src/model/entities/Department.java
@@ -0,0 +1,72 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Department implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer Id;
+	private String Name;
+	
+	public Department() {
+		
+	}
+
+	public Department(Integer id, String name) {
+		Id = id;
+		Name = name;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		if (Name == null) {
+			if (other.Name != null)
+				return false;
+		} else if (!Name.equals(other.Name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [Id=" + Id + ", Name=" + Name + "]";
+	}
+}
