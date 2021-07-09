7f97930494f73a23b0b469fe582265456880b039
Mauricio PC Casa <mauriciozaccaro@gmail.com>
2021-07-08 22:53:21 -0400

    Retirado o Loader2 e adicionado toda sua lógica como um PARAMETRO da funcção Loader() principal utilizando Consumer, Generics e expressões Lambda

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,12 +36,16 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+			
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml"); // chama a tela About.fxml 
+		loadView("/gui/About.fxml", x -> {}); // chama a tela About.fxml 
 	}
 	
 	@Override
@@ -49,7 +54,7 @@ public class MainViewController implements Initializable {
 		
 	}
 	
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		// esse "synchronized" serve para não dar B.O com as multi-trheds durante a executação das telas
 		/* Toda essa função loadView é para quando chamar uma outra cena (Scene) ele mudar os "filhos" sem mudar o tipo da Cena, 
 		 *  algo assim, não entendi direito direito... quando entender mais eu volto a escrever...
@@ -67,39 +72,11 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu); // pega o primeiro filho que ficou armazenado no mainMenu e adiciona ao mainVBox novamente
 			mainVBox.getChildren().addAll(newVBox.getChildren()); // adiciona todos os filhos do newVBox que é a outra tela VBox
 			
-			
-			
-		}catch(IOException e) {
-			Alerts.showAlerts("IOException", "Erro ao carregar a tela", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
-		// esse "synchronized" serve para não dar B.O com as multi-trheds durante a executação das telas
-		/* Toda essa função loadView é para quando chamar uma outra cena (Scene) ele mudar os "filhos" sem mudar o tipo da Cena, 
-		 *  algo assim, não entendi direito direito... quando entender mais eu volto a escrever...
-		 * */
-		try {
-			
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			Node mainMenu = mainVBox.getChildren().get(0); // passa para o Node mainMenu o primeiro (0) filho do mainVBox
-			mainVBox.getChildren().clear(); // limpa todos os filhos do mainVBox
-			mainVBox.getChildren().add(mainMenu); // pega o primeiro filho que ficou armazenado no mainMenu e adiciona ao mainVBox novamente
-			mainVBox.getChildren().addAll(newVBox.getChildren()); // adiciona todos os filhos do newVBox que é a outra tela VBox
-			
-			DepartmentListController controller = loader.getController();
-			
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 			
 		}catch(IOException e) {
 			Alerts.showAlerts("IOException", "Erro ao carregar a tela", e.getMessage(), AlertType.ERROR);
 		}
 	}
-
 }
