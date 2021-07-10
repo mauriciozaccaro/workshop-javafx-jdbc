80480454620104c2f438d110c0bae4ddf9edc63a
Mauricio PC Casa <mauriciozaccaro@gmail.com>
2021-07-09 23:06:05 -0400

    Adicionado as colunas de BirthDate, BaseSalary e Email ao SellerList.fxml

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -48,6 +49,15 @@ public class SellerListController implements Initializable, DataChangeListener{
 	private TableColumn<Seller, String> tableColumnName;
 	
 	@FXML
+	private TableColumn<Seller, String> tableColumnEmail;
+
+	@FXML
+	private TableColumn<Seller, Date> tableColumnBirthDate;
+
+	@FXML
+	private TableColumn<Seller, Double> tableColumnBaseSalary;
+	
+	@FXML
 	private TableColumn<Seller, Seller> tableColumnEdit;
 	
 	@FXML
@@ -72,7 +82,12 @@ public class SellerListController implements Initializable, DataChangeListener{
 	
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id")); // ainda não sei para que serve... fiquei em duvida
-		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+		tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+		Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 		
 		Stage stage = (Stage) Main.getMainScene().getWindow(); 
 		/* basicamente, está pegando a propriedade de width do Stage 
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,26 +1,75 @@
 package gui.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class Utils {
 
-	public static Stage currentStage(ActionEvent event) { //currentStage = Evento atual ou Palco atual
-		return (Stage) ((Node) event.getSource()).getScene().getWindow();/* aqui é feito uma bagunça.... tipo, a gente pega o 
-		event;getSource(), mas ele é muito genérico então fazemos um downCast para o tipo "Node" e a partir desse Node chamamos 
-		o getScene().getWindow()  que é para pegar tela da cena. Como esse getWindow é uma SUPER classe do Stage, a gente faz um 
-		novo DownCast para o tipo "Stage" */
+	public static Stage currentStage(ActionEvent event) { // currentStage = Evento atual ou Palco atual
+		return (Stage) ((Node) event.getSource()).getScene()
+				.getWindow();/*
+								 * aqui é feito uma bagunça.... tipo, a gente pega o event;getSource(), mas ele
+								 * é muito genérico então fazemos um downCast para o tipo "Node" e a partir
+								 * desse Node chamamos o getScene().getWindow() que é para pegar tela da cena.
+								 * Como esse getWindow é uma SUPER classe do Stage, a gente faz um novo DownCast
+								 * para o tipo "Stage"
+								 */
 	}
-	
+
 	public static Integer tryParseToInt(String str) {
-		/* Esse método é para tratar o que vem láaaaa da TextField do Id, para que ele converta 
-		 * o texto (String) do TextField para um numero Inteiro. E, caso não seja possível fazer tal 
-		 * conversão, vai return null */
+		/*
+		 * Esse método é para tratar o que vem láaaaa da TextField do Id, para que ele
+		 * converta o texto (String) do TextField para um numero Inteiro. E, caso não
+		 * seja possível fazer tal conversão, vai return null
+		 */
 		try {
 			return Integer.parseInt(str);
-		}catch(NumberFormatException e ) {
+		} catch (NumberFormatException e) {
 			return null;
 		}
+	}
+
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+			return cell;
+		});
+	}
+
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+			return cell;
+		});
 	}
 }
